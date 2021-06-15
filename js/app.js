58e59e4d8267bc58b857ccceeba24a8a75988d9d
Erik G! <erikdotg@gmail.com>
2021-06-15 11:59:44 -0400

    uses border-box to fix sizing issue, adds classes to distinguis living from dead cells

--- js/app.js
+++ js/app.js
@@ -3,18 +3,32 @@ let board = document.getElementById('board')
 let width = 1000
 let height = 1000
 let cellSize = 100
-let cell
+
 
 
 window.onload = function makeBoard() {
 for (let n = 1; n <= (width * height) / cellSize; n++) {
 
-    cell = document.createElement("div");
+    let cell = document.createElement("div");
     cell.setAttribute('id', n)
     cell.classList.add('cell');
     board.appendChild(cell)
     // console.log(n)
 }
-
     
 }
+
+function toggleCell() {
+    alert('toggleCell function activated');
+    let cell = document.getElementsByClassName('cell');
+    if ('alive' in cell.classList) {
+        cell.classList.remove('alive');
+    } else {
+        cell.classList.add('alive');
+    }
+
+}
+
+const thisCell = document.getElementsByClassName('cell')
+
+thisCell.addEventListener('click', toggleCell)
